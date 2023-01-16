f89f9a1624a0b536889c602617470a65dad5825f
zinseo964 <wlstj9964@gmail.com>
2023-01-16 19:31:36 +0900

    0116 java Spring practice Create New Java Module

---
+++ Bt09_MVC/src/main/java/com/lec/spring/Bt09MvcApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt09MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt09MvcApplication.class, args);
+	}
+
+}
---
+++ Bt09_MVC/src/test/java/com/lec/spring/Bt09MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt09MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
